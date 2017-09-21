cc71cecfe8e27980e18f6189f731c6f578d198fc
xiongfeng <idioticbear666@gmail.com>
2017-09-21 19:02:08 +0800

    chapter05

---
+++ chapter05/src/main/java/com/robert/chapter05/shader/ShaderActivity.java
@@ -0,0 +1,22 @@
+package com.robert.chapter05.shader;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.robert.chapter05.R;
+
+/**
+ * @author: robert
+ * @date: 2017-09-21
+ * @time: 18:58
+ * @说明:
+ */
+public class ShaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shader);
+    }
+}
