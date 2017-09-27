b224c59ff5383f429c5f6d99347ade95754ea913
robert <idioticmadman@qq.com>
2017-09-27 08:19:29 +0800

    circleview

---
+++ chapter05/src/main/java/com/robert/chapter05/pratice/CirclePhotoView.java
@@ -0,0 +1,8 @@
+package com.robert.chapter05.pratice;
+
+/**
+ * Created by robert on 2017/9/26.
+ */
+
+public class CirclePhotoView {
+}
---
+++ chapter05/src/main/java/com/robert/chapter05/pratice/PracticeActivity.java
@@ -0,0 +1,10 @@
+package com.robert.chapter05.pratice;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by robert on 2017/9/26.
+ */
+
+public class PraticeActivity extends AppCompatActivity {
+}
